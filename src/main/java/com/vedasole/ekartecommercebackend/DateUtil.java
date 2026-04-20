e62889b3828567e5414061b5f00ab52a980a9446
Elisha Wong Peh Jie <elishawong@1utar.my>
2026-04-20 20:52:13 +0800

    add utility class,logging and input validation

---
+++ src/main/java/com/vedasole/ekartecommercebackend/DateUtil.java
@@ -0,0 +1,12 @@
+package com.vedasole.ekartecommercebackend;
+
+public class DateUtil {
+
+    public static String getCurrentYear() {
+        return "2026";
+    }
+
+    public static String getProjectName() {
+        return "eKart Backend System";
+    }
+}
